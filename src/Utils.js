e7491e2a9eca563bca787961e60822d6db238691
Erin Wolff <erin_wolff@outlook.com>
2024-02-28 16:20:28 -0800

    deleted Utils, no longer needed

--- src/Utils.js
+++
@@ -1,8 +0,0 @@
-const Utils = {
-  months: (count) => {
-    let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-    return months.slice(0, count);; 
-  }
-};
-
-export default Utils;
