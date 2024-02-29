778ab857d1f31ae81dec46ddacb0b6b9c0e8a7ea
Erin Wolff <erin_wolff@outlook.com>
2024-02-28 16:21:46 -0800

    passed filteredGames prop to display peak CCU data

--- src/BarChart.jsx
+++ src/BarChart.jsx
@@ -1,39 +1,38 @@
 import React, { useEffect, useRef } from 'react';
 import { Chart as ChartJS, BarElement, CategoryScale, LinearScale, BarController } from 'chart.js/auto';
-import Utils from './Utils.js';
 
 ChartJS.register(BarElement, CategoryScale, LinearScale, BarController);
 
 
-export default function BarChart() {
+export default function BarChart({ filteredGames }) {
   const chartRef = useRef(null);
+  const peakCCUData = filteredGames.map(game => game["Peak CCU"]);
 
   let chartInstance = null; // Store the chart instance
 
-  const labels = Utils.months(7);
+  const labels = filteredGames.map(game => game["Name"]);
+
 
   const data = {
     labels: labels,
     datasets: [{
-      label: 'My First Dataset',
-      data: [65, 59, 80, 81, 56, 55, 40],
+      label: 'Peak CCU',
+      data: peakCCUData,
+      maxBarThickness: 40,
       backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
-        'rgba(255, 205, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(201, 203, 207, 0.2)'
+        'rgba(255, 99, 132, 0.6)',
+        'rgba(255, 159, 64, 0.6)',
+        'rgba(255, 205, 86, 0.6)',
+        'rgba(75, 192, 192, 0.6)',
+        'rgba(54, 162, 235, 0.6)',
+        'rgba(153, 102, 255, 0.6)',
+        'rgba(36, 46, 216, 0.6)',
+        'rgba(245, 40, 145, 0.6)',
+        'rgba(201, 203, 200, 0.6)',
+        'rgba(201, 203, 64, 0.6)',
       ],
       borderColor: [
-        'rgb(255, 99, 132)',
-        'rgb(255, 159, 64)',
-        'rgb(255, 205, 86)',
-        'rgb(75, 192, 192)',
-        'rgb(54, 162, 235)',
-        'rgb(153, 102, 255)',
-        'rgb(201, 203, 207)'
+        'rgb(53, 53, 57)',
       ],
       borderWidth: 1
     }]
@@ -43,12 +42,28 @@ export default function BarChart() {
     type: 'bar',
     data: data,
     options: {
+      tooltips: {
+        enabled: true,
+        callbacks: {
+          label: function (tooltipItem, data) {
+            let datasetLabel = data.datasets[tooltipItem.datasetIndex].label || '';
+            let value = data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index];
+            return datasetLabel + ': ' + value;
+          }
+        }
+      },
       scales: {
         y: {
-          beginAtZero: true
+          beginAtZero: true,
+          ticks: {
+            color: '#000'
+          },
         },
         x: {
           type: 'category',
+          ticks: {
+            color: '#000'
+          },
         }
       },
       responsive: true,
@@ -69,7 +84,7 @@ export default function BarChart() {
       chartInstance.destroy(); // Cleanup on unmount
 
     }
-  }, []);
+  }, [filteredGames]);
 
   return (
     <div>
