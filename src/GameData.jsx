f0de35adf51416143da49f0409159df659195e44
Erin Wolff <erin_wolff@outlook.com>
2024-03-13 18:46:11 -0700

    added input for user to provide steamID for custom data retrieval

--- src/GameData.jsx
+++ src/GameData.jsx
@@ -1,23 +1,23 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import TextField from '@mui/material/TextField';
+import Tooltip from '@mui/material/Tooltip';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Unstable_Grid2';
 import BarChart from './BarChart.jsx';
 import Button from '@mui/material/Button';
 
-function getRandomGames(gamesArray, count = 15) {
+function getRandomGames(gamesArray, count = 10) {
   const shuffled = gamesArray.sort(() => 0.5 - Math.random()); // Shuffle the array
   return shuffled.slice(0, count);
 }
 
-
-
 export default function GameData() {
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   const [filteredGames, setFilteredGames] = useState([]);
   const [steamUserData, setSteamUserData] = useState({ response: { games: [] } });
   const [randomGames, setRandomGames] = useState([]);
+  const [steamId, setSteamId] = useState('');
 
   const refreshRandomGames = () => {
     const randomSelection = getRandomGames(steamUserData.response.games);
@@ -25,30 +25,27 @@ export default function GameData() {
   };
 
 
-  useEffect(() => {
-    const fetchSteamData = async () => {
-      try {
-        const response = await fetch('http://localhost:80/api/games');
-        const data = await response.json();
-        setSteamUserData(data);
-        setIsLoading(false);
-        if (data?.response?.games?.length > 0 && randomGames.length === 0) {
-          const randomSelection = getRandomGames(data.response.games);
-          setRandomGames(randomSelection);
-        }
-      } catch (error) {
-        console.log('Error fetching Steam data: ', error);
+  const fetchSteamData = async (steamId) => {
+    try {
+      setIsLoading(true);
+      const response = await fetch(`http://localhost:80/api/games?steamId=${steamId}`);
+      const data = await response.json();
+      setSteamUserData(data);
+      setIsLoading(false);
+      if (data?.response?.games?.length > 0 && randomGames.length === 0) {
+        const randomSelection = getRandomGames(data.response.games);
+        setRandomGames(randomSelection);
       }
-    };
-    fetchSteamData();
-  }, []);
-
+    } catch (error) {
+      console.log('Error fetching Steam data: ', error);
+    }
+  };
 
   const handleSearch = (e) => {
     const value = e.target.value.toLowerCase(); // Normalize for case-insensitive search
 
     if (value === '') {
-      setFilteredGames(randomGames); // Reset to full game list
+      setFilteredGames([]); // clear the search results
     } else {
       const newFilteredGames = steamUserData.response.games.filter((game) =>
         game.name?.toLowerCase().includes(value) // Search by 'name'
@@ -59,9 +56,7 @@ export default function GameData() {
 
   const GameCardContainer = ({ displayGames }) => {
     return (
-      
       <Box sx={{ flexGrow: 1 }}>
-        <h1>Playtime</h1>
         <Grid className="game-container" container spacing={5} columns={{ xs: 1, sm: 1, md: 13 }}>
           {displayGames.map((game) => (
             <Grid className="game-card" key={game.appid} xs={1} sm={2} md={2}>
@@ -80,19 +75,43 @@ export default function GameData() {
     );
   };
 
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      fetchSteamData(steamId); // Trigger data fetch with steamId
+      setSteamId(''); // Clear the input field
+    }
+  };
+
   return (
     <div>
-      <Button variant="outlined" color="secondary" size="small" sx={{ margin: '10px' }} onClick={refreshRandomGames}>Refresh</Button>
       <div className="search-bar">
         <TextField
           id="filled-search"
-          label="Search field"
+          label="Search Game Titles"
           type="search"
           variant="outlined"
           color="success"
           onChange={handleSearch}
         />
       </div>
+      <div className="steam-id-input">
+        <Tooltip placement="right-start" title="To find your Steam ID, visit your Steam User Profile. Your ID is the number at the end of your profile URL: https://steamcommunity.com/profiles/YOUR-STEAM-ID/">
+          <TextField
+            id="steam-id-input"
+            label="Steam ID"
+            type="input"
+            variant="outlined"
+            value={steamId}
+            color="success"
+            onChange={(e) => setSteamId(e.target.value)}
+            onKeyDown={handleKeyDown}
+            helperText="Press ENTER to submit"
+          />
+        </Tooltip>
+      </div>
+      {isLoading && <p>Loading game data ...</p>}
+      {error && <p>{error}</p>}
+      <br />
       <div className="chart-container">
         {filteredGames.length > 0 ? (
           <BarChart filteredGames={filteredGames} />
@@ -100,16 +119,18 @@ export default function GameData() {
           <BarChart filteredGames={randomGames} />
         )}
       </div>
-      {isLoading && <p>Loading game data ...</p>}
-      {error && <p>{error}</p>}
-      {steamUserData &&
-        (filteredGames.length > 0
-          ? <GameCardContainer displayGames={filteredGames} />  // Show filtered results
-          : <GameCardContainer displayGames={randomGames} />// Only show games with playtime >= 30 hours
-        )
+      <br />
+      <Button variant="outlined" color="secondary" size="small" sx={{ margin: '10px' }} onClick={refreshRandomGames}>Refresh</Button>
+      <br />
+      <br />
+      <h3>User Stats</h3>
+      <p>Total Games Owned: {steamUserData.response.game_count}</p>
+      <br />
+      <br />
+      {steamUserData && filteredGames.length > 0 && (
+        <GameCardContainer displayGames={filteredGames} />
+      )
       }
     </div>
   );
 }
-
-// add input to allow other users to input their steamid 
