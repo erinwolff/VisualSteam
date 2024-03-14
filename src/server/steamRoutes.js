b21093c5151bb5f1d40d6ce8e3ec0b057a4a01a0
Erin Wolff <erin_wolff@outlook.com>
2024-03-13 23:18:17 -0700

    added route to fetch userSummary & enhanced error handling

--- src/server/steamRoutes.js
+++ src/server/steamRoutes.js
@@ -14,8 +14,29 @@ router.get('/games', async (req, res) => {
     });
     res.json(response.data);
   } catch (error) {
-    // ... error handling
+    console.error('Error fetching Owned Games summary:', error);
+    console.error('Error Response:', error.response); 
+    res.status(500).json({ error: 'Failed to fetch Owned Games summary' });
   }
+});
+
+router.get('/userSummary', async (req, res) => {
+  try {
+    const STEAM_ID = req.query.steamId 
+    const apiKey = process.env.STEAM_API_KEY;
+
+    const response = await axios.get(`https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${apiKey}&steamids=${STEAM_ID}&format=json&include_appinfo=1`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${apiKey}`,
+      },
+    });
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error fetching Steam user summary:', error);
+    console.error('Error Response:', error.response); 
+    res.status(500).json({ error: 'Failed to fetch Steam user summary' });
+}
 });
 
 module.exports = router;
