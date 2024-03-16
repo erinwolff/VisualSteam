26aa82855bc08670b87e4f38d2ad7b27028addb6
Erin Wolff <erin_wolff@outlook.com>
2024-03-15 17:26:32 -0700

    added new route to get recently played games

--- src/server/steamRoutes.js
+++ src/server/steamRoutes.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 router.get('/games', async (req, res) => {
   try {
-    const STEAM_ID = req.query.steamId 
+    const STEAM_ID = req.query.steamId
     const apiKey = process.env.STEAM_API_KEY;
 
     const response = await axios.get(`https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${apiKey}&steamid=${STEAM_ID}&format=json&include_appinfo=1`, {
@@ -15,14 +15,14 @@ router.get('/games', async (req, res) => {
     res.json(response.data);
   } catch (error) {
     console.error('Error fetching Owned Games summary:', error);
-    console.error('Error Response:', error.response); 
+    console.error('Error Response:', error.response);
     res.status(500).json({ error: 'Failed to fetch Owned Games summary' });
   }
 });
 
 router.get('/userSummary', async (req, res) => {
   try {
-    const STEAM_ID = req.query.steamId 
+    const STEAM_ID = req.query.steamId
     const apiKey = process.env.STEAM_API_KEY;
 
     const response = await axios.get(`https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${apiKey}&steamids=${STEAM_ID}&format=json&include_appinfo=1`, {
@@ -34,9 +34,28 @@ router.get('/userSummary', async (req, res) => {
     res.json(response.data);
   } catch (error) {
     console.error('Error fetching Steam user summary:', error);
-    console.error('Error Response:', error.response); 
+    console.error('Error Response:', error.response);
     res.status(500).json({ error: 'Failed to fetch Steam user summary' });
-}
+  }
+});
+
+router.get('/recentlyPlayedGames', async (req, res) => {
+  try {
+    const STEAM_ID = req.query.steamId
+    const apiKey = process.env.STEAM_API_KEY;
+
+    const response = await axios.get(`https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=${apiKey}&steamid=${STEAM_ID}&format=json`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${apiKey}`,
+      },
+    });
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error fetching Recently Played Games summary:', error);
+    console.error('Error Response:', error.response);
+    res.status(500).json({ error: 'Failed to fetch Recently Played Games summary' });
+  }
 });
 
 module.exports = router;
