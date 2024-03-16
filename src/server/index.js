8935962d78c085f42fbf35b6696ca6d9d61ca476
Erin Wolff <erin_wolff@outlook.com>
2024-03-15 23:16:17 -0700

    added const path

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const path = require('path'); 
 
 const PORT = process.env.PORT ?? 80;
 
